61cbe12e1d203657b759ee4378c412ef7bc86ca4
Michal Naka <michal.naka@gmail.com>
2026-07-18 20:42:55 -0400

    Fix guide styling on preview

--- scripts/render_guides.py
+++ scripts/render_guides.py
@@ -207,7 +207,7 @@ def render_guide(guide: dict) -> str:
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,400..800&family=Instrument+Sans:wght@400;500;600&family=Space+Mono:wght@400;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="/assets/guide.css">
+<link rel="stylesheet" href="../../assets/guide.css">
 <script type="application/ld+json">
 {schema_for(guide)}
 </script>
